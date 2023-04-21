b4eedc0bd360391f0edd4e5dfb143cc8528ab583
Enfield-Li <88321505+Enfield-Li@users.noreply.github.com>
2023-04-21 11:10:30 +0800

    password field

--- app/auth/sign_in_with_password.tsx
+++ app/auth/sign_in_with_password.tsx
@@ -2,11 +2,13 @@ import { Stack } from "expo-router";
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/Feather";
 
 type Props = {};
 
 function sign_in_with_password(props: Props) {
   const [password, setPassword] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
 
   return (
     <View className="flex-1 items-center">
@@ -19,21 +21,45 @@ function sign_in_with_password(props: Props) {
         }}
       />
 
-      <View>
-        <Text>Password</Text>
-        <TextInput
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
+      <View className="w-full p-3">
+        <Text className="text-gray-700 text-xs">Password</Text>
+
+        <View className="flex-row border border-gray-300 rounded-lg mt-2 mb-5 p-2 pl-3 justify-between items-center">
+          <View className="flex-row items-center flex-1 pr-4">
+            <Icon size={17} name="lock" color="gray" />
+
+            <TextInput
+              autoFocus
+              value={password}
+              className="ml-2 flex-1"
+              placeholder="Password"
+              secureTextEntry={hidePassword}
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+
+          <TouchableOpacity
+            className="p-1"
+            onPress={() => setHidePassword(!hidePassword)}
+          >
+            {hidePassword ? (
+              <Icon size={17} name="eye-off" color="gray" />
+            ) : (
+              <Icon size={17} name="eye" color="gray" />
+            )}
+          </TouchableOpacity>
+        </View>
 
         <TouchableOpacity
           //   href="my_test"
-          className="w-11/12 bg-purple-500 py-3 rounded-lg mx-auto mb-6 text-center"
+          className="w-full bg-purple-600 py-3 rounded-lg mx-auto mb-4 text-center"
         >
-          <Text className="text-white">Get started</Text>
+          <Text className="text-white text-center">Sign in</Text>
         </TouchableOpacity>
-        <TouchableOpacity className="w-11/12 bg-purple-500 py-3 rounded-lg mx-auto mb-6 text-center">
-          <Text>Forgot password?</Text>
+        <TouchableOpacity>
+          <Text className="text-purple-500 text-center font-semibold text-xs">
+            Forgot password?
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
